3d5e9ee0813e6ed5976a98a2356ff12ac865b36f
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-08-18 19:51:31 +0900

    업로드구현

--- SleepingLetter/MainActivity.cs
+++ SleepingLetter/MainActivity.cs
@@ -1,19 +1,54 @@
-﻿using Android.App;
+﻿using System.Net;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using System.Text;
+using Android.Util;
+using System.Threading;
 
 namespace SleepingLetter
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText Letter;
+        Button Sendbutton;
+        letter letter;
+        
+        Toast toast;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            // Set our view from the "main" layout resource
+            Letter = FindViewById<EditText>(Resource.Id.textInputEditText1);
+            Sendbutton = FindViewById<Button>(Resource.Id.sendLetter);
+            Sendbutton.Click += Sendbutton_ClickAsync;
+            letter = new letter();
+
+        }
+
+        
+
+        async void Sendbutton_ClickAsync(object sender, EventArgs e)
+        {
+            letter.Message = Letter.Text;
+            var req = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://192.168.56.1:12358"),
+                Content = new StringContent(JsonConvert.SerializeObject(letter), Encoding.UTF8, "application/json")
+            };
+
+            var client = new HttpClient();
+            var res = await client.PutAsync(req.RequestUri, req.Content);
+            Log.Debug("HTTP", res.Content.ReadAsStringAsync().ToString());
+              
+          
         }
     }
 }
